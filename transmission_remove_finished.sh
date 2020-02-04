1f2779bc3e865e7b061e83580fb200bc4ff45554
Francisco <rebolledo.francisco@gmail.com>
2020-02-04 19:26:25 -0300

    fix script

--- transmission_remove_finished.sh
+++ transmission_remove_finished.sh
@@ -2,7 +2,7 @@
 
 # port, username, password
 SERVER="localhost:9091 --auth fran:topo01"
-MAX_RATIO=0
+MAX_RATIO=1
 
 # use transmission-remote to get torrent list from transmission-remote list
 TORRENTLIST=`transmission-remote $SERVER --list | sed -e '1d' -e '$d' | awk '{print $1}' | sed -e 's/[^0-9]*//g'`
@@ -15,7 +15,7 @@ do
     RATIO=`cat $INFO | grep 'Ratio:' | awk '{print $2}'`
     NAME=$(cat $INFO | sed -e 's/.*Name: \(.*\) Hash.*/\1/')
     #echo -e "Processing #$TORRENTID - $(cat $INFO | sed -e 's/.*Name: \(.*\) Hash.*/\1/')"
-    echo "Processing #$TORRENTID -"
+    echo "Processing #$TORRENTID $NAME-"
     
     # check if torrent download is completed
     DL_COMPLETED=`cat $INFO | grep "Done: 100%"`
@@ -24,10 +24,10 @@ do
 
     # if the torrent is "Stopped", "Finished", or "Idle after downloading 100%"
     if [ "$DL_COMPLETED" ] && [ "$STATE_STOPPED" ] && [ $(echo "$RATIO >= $MAX_RATIO" | bc -l) -eq 1 ]; then
-        echo "Torrent #$TORRENTID is completed. Removing torrent from list."
+        echo "Torrent #$TORRENTID $NAME is completed. Removing torrent from list."
         transmission-remote $SERVER --torrent $TORRENTID --remove-and-delete
     else
-        echo "Torrent #$TORRENTID is not completed. Ignoring."
+        echo "Torrent #$TORRENTID $NAME is not completed. Ignoring."
     fi
 done
 
